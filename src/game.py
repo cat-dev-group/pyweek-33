5359f7f0518bdf2a5edba872111bff92b4f3ac8a
bradtimmis <bradleytimmis@gmail.com>
2022-03-23 18:53:58 -0400

    Give Player 1 a navy color - sorry Oreo

--- src/game.py
+++ src/game.py
@@ -50,7 +50,7 @@ class Player(pyglet.shapes.Rectangle):
 
 
 player = Player(
-    x=0, y=HEIGHT // 6, ground=HEIGHT // 6, width=40, height=40, color=(240, 100, 50)
+    x=0, y=HEIGHT // 6, ground=HEIGHT // 6, width=40, height=40, color=(0, 0, 128)
 )
 player2 = Player(
     x=0,
