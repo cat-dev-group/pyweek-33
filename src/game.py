c5b6f8a25ce6559d6b0304ea0e83e1f368cfc46e
Oreo <80539149+Oreo4102004@users.noreply.github.com>
2022-03-25 23:42:15 +0530

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,10 +1,6 @@
 import pyglet
-from .level_tools import Player
+from .level_tools import Player, platform, flag, button
 from .constants import WIDTH, HEIGHT
-
-# display = pyglet.canvas.Display()
-# screen = display.get_default_screen()
-# WIDTH, HEIGHT = screen.width, screen.height
 
 bg = pyglet.resource.image("bg.png")
 
@@ -24,22 +20,38 @@ evil_twin = Player(
     color=(235, 64, 52),
 )
 
+
 # def on():
-#     plats[-1].y = -100
+#     Player.entities["platform"][WIDTH // 2, HEIGHT // 2 + HEIGHT // 6].y = -100
+
+
 # def off():
-#     plats[-1].y = HEIGHT//6 + HEIGHT//2
+#     Player.entities["platform"][WIDTH // 2, HEIGHT // 2 + HEIGHT // 6].y = (
+#         HEIGHT // 6 + HEIGHT // 2
+#     )
+
 
 game_window.push_handlers(good_twin)
 game_window.push_handlers(evil_twin)
 game_window.push_handlers(good_twin.key_handler)
 game_window.push_handlers(evil_twin.key_handler)
-# land_obj = platform(WIDTH - WIDTH//3,HEIGHT//4.5,width = WIDTH//6,color = (165, 42, 42))
-# land_obj2 = platform(WIDTH - WIDTH//2,HEIGHT//4,width = WIDTH//6,color = (165, 42, 42))
-# land_obj3 = platform(WIDTH//2,HEIGHT//2+HEIGHT//6,width = HEIGHT//2,color = (165, 42, 42),alignment = 'v')
-# plats = [land_obj,land_obj2,land_obj3]
-# f1 = flag(WIDTH-100,HEIGHT//2+HEIGHT//6)
-# f2 = flag(WIDTH-100,HEIGHT//6)
-# b = button(WIDTH//3,HEIGHT//6 + 50,on,off)
+# platform(
+#     WIDTH - WIDTH // 3, HEIGHT // 4.5, width=WIDTH // 6, color=(165, 42, 42)
+# )
+# platform(
+#     WIDTH - WIDTH // 2, HEIGHT // 4, width=WIDTH // 6, color=(165, 42, 42)
+# )
+# platform(
+#     WIDTH // 2,
+#     HEIGHT // 2 + HEIGHT // 6,
+#     width=HEIGHT // 2,
+#     color=(165, 42, 42),
+#     alignment="v",
+# )
+
+# flag(WIDTH - 100, HEIGHT // 2 + HEIGHT // 6)
+# flag(WIDTH - 100, HEIGHT // 6)
+# button(WIDTH // 3, HEIGHT // 6 + 50, on, off)
 
 
 def render_entities():
@@ -54,12 +66,14 @@ def on_draw():
     game_window.clear()
     bg.blit(0, 0)
 
-    # render_entities()
+    render_entities()
 
 
 def update(dt):
-    good_twin.update(dt)
-    evil_twin.update(dt)
+    for i, j in Player.entities.items():
+        for obj in j.values():
+            if i in ("button", "players"):
+                obj.update(dt)
 
 
 # driver code
